c409732ccd5589268d92e8a36e0310a0076c52a2
even.chang <even1101u@gmail.com>
2022-09-02 16:54:59 +0800

    refactor repository and run shell

--- banking/app/app.go
+++ banking/app/app.go
@@ -3,10 +3,14 @@ package app
 import (
 	"banking/domain"
 	"banking/service"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
 )
 
 func Start() {
@@ -15,10 +19,28 @@ func Start() {
 
 	router.HandleFunc("/ping", ping).Methods(http.MethodGet)
 
-	ch := CustomerHandlers{service.NewCustomerService(domain.NewCustomerRepositoryDb())}
+	dbClient := getDbClient()
+	customerRepositoryDb := domain.NewCustomerRepositoryDb(dbClient)
+
+	ch := CustomerHandlers{service.NewCustomerService(customerRepositoryDb)}
 	router.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
 
-	log.Fatal(http.ListenAndServe("localhost:9527", router))
+	host := os.Getenv("SERVER_HOST")
+	port := os.Getenv("SERVER_PORT")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", host, port), router))
+	// log.Fatal(http.ListenAndServe("localhost:9527", router))
 
+}
+
+func getDbClient() *sqlx.DB {
+	client, err := sqlx.Open("mysql", "user:pwd@tcp(localhost:3306)/banking")
+	if err != nil {
+		panic(err)
+	}
+	// See "Important settings" section.
+	client.SetConnMaxLifetime(time.Minute * 3)
+	client.SetMaxOpenConns(10)
+	client.SetMaxIdleConns(10)
+	return client
 }
